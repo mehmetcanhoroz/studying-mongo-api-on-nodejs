aec5621f52c10d0323219588ff6f58aa766d444c
Mehmet CANHOROZ <mehmetcanhoroz@gmail.com>
2018-09-13 18:20:37 +0300

    Studied Find, Delete MongoDB

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,5 +1,5 @@
 //const MongoClient = require("mongodb").MongoClient;
-const {MongoClient, ObjectID} = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
 
 MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true }, (err, client) => {
     if (err) {
@@ -9,11 +9,24 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true
     console.log("Successfully connected to Db server!");
 
 
+    /*db.collection("Todos").find({
+        _id: new ObjectID("5b967ab5f3fb380e8db55b2e")
+    }).toArray().then((docs) => {
+        console.log("Todos");
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log("Error occurred fetching todos ", err);
+    });*/
+
+    db.collection("Todos").find().count().then((count) => {
+        console.log("Todos count:",count);
+    }, (err) => {
+        console.log("Error occurred fetching todos ", err);
+    });
 
 
     client.close((err) => {
-        if(err)
-        {
+        if (err) {
             return console.log("Error has occurred while closing conneciton!");
         }
         console.log("Connection closed!");
